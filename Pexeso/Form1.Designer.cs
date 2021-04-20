bfd1c8674a314769b60e4975d57b4c13e8a4aa9e
Patrik Trefil <patrik.trefil@gmail.com>
2021-04-20 15:12:29 +0200

    first working version

--- Pexeso/Form1.Designer.cs
+++ Pexeso/Form1.Designer.cs
@@ -35,29 +35,35 @@ namespace Pexeso
             // 
             // buttonStart
             // 
-            this.buttonStart.Location = new System.Drawing.Point(237, 34);
+            this.buttonStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStart.BackColor = System.Drawing.Color.Green;
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonStart.ForeColor = System.Drawing.Color.Black;
+            this.buttonStart.Location = new System.Drawing.Point(139, 60);
             this.buttonStart.Name = "buttonStart";
-            this.buttonStart.Size = new System.Drawing.Size(343, 129);
+            this.buttonStart.Size = new System.Drawing.Size(500, 100);
             this.buttonStart.TabIndex = 0;
             this.buttonStart.Text = "Start Game";
-            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.UseVisualStyleBackColor = false;
             this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             // 
             // buttonQuit
             // 
-            this.buttonQuit.Location = new System.Drawing.Point(237, 224);
+            this.buttonQuit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.buttonQuit.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold);
+            this.buttonQuit.Location = new System.Drawing.Point(218, 217);
             this.buttonQuit.Name = "buttonQuit";
             this.buttonQuit.Size = new System.Drawing.Size(343, 147);
             this.buttonQuit.TabIndex = 1;
             this.buttonQuit.Text = "Quit Game";
-            this.buttonQuit.UseVisualStyleBackColor = true;
+            this.buttonQuit.UseVisualStyleBackColor = false;
             this.buttonQuit.Click += new System.EventHandler(this.buttonQuit_Click);
             // 
             // StartScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(778, 444);
             this.Controls.Add(this.buttonQuit);
             this.Controls.Add(this.buttonStart);
             this.Name = "StartScreen";
--- Pexeso/GameForm.cs
+++ Pexeso/GameForm.cs
@@ -7,21 +7,89 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Card = System.Int32;
 
 namespace Pexeso
 {
     public partial class GameForm : Form
     {
-        private StartScreen startScreen;
+        private readonly StartScreen startScreen;
+        private readonly Card[] playingCards = { 1, 2, 3, 4, 5, 6, 7, 8 }; // set content of cards here
+        private readonly Card[] cards;
+        private Button firstCard, secondCard;
+        private readonly Random rnd = new Random();
+        private State state = State.noCardsOpened;
         public GameForm(StartScreen startScreen)
         {
             this.startScreen = startScreen;
+
+            // create pairs of cards
+            cards = new Card[playingCards.Length * 2];
+            for (int i = 0; i < cards.Length; i++)
+            {
+                cards[i] = playingCards[i / 2];
+            }
+
+            randomOrder(ref cards);
+
             InitializeComponent();
         }
 
         private void GameForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             startScreen.Show();
+        }
+
+        private void cardClick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int index = btn.TabIndex;
+            switch (state)
+            {
+                case State.oneCardOpened:
+                    if (btn != firstCard)
+                    {
+                        secondCard = btn;
+                        btn.Text = cards[index].ToString();
+                        state = State.twoCardsOpened;
+                    }
+                    break;
+                case State.twoCardsOpened:
+                    if (firstCard.Text == secondCard.Text)
+                    {
+                        firstCard.Hide();
+                        secondCard.Hide();
+                    }
+                    else
+                    {
+                        firstCard.Text = "";
+                        secondCard.Text = "";
+                    }
+                    state = State.noCardsOpened;
+                    break;
+                case State.noCardsOpened:
+                    firstCard = btn;
+                    btn.Text = cards[index].ToString();
+                    state = State.oneCardOpened;
+                    break;
+            }
+        }
+
+        private void randomOrder(ref Card[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int j = rnd.Next(0, i);
+                Card temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        public enum State
+        {
+            oneCardOpened,
+            twoCardsOpened,
+            noCardsOpened
         }
     }
 }
